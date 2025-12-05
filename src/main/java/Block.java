0edb50e4d2646657213e861a5e6cf9b1ac445957
L1za-T <78148912+L1za-T@users.noreply.github.com>
2025-12-05 10:40:29 +0000

    Initial commit.

---
+++ src/main/java/Block.java
@@ -0,0 +1,6 @@
+import java.util.Vector;
+public class Block {
+    double mass;
+    double[] pos = new double[3];
+    double[] vel = new double[3];
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+import java.util.Vector;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+/*
+* Things needed:
+* Initial conditions:
+* mass, gravity, static and kinetic friction
+*
+* Normal vector
+*
+* fgn[0], fgp[0], fn[0]
+*
+* a check if the force is static or kinetic
+*
+* if kinetic, find direction of acceleration and rate of acceleration
+* (i *think* euler's only applies to the acceleration and position)
+* */
